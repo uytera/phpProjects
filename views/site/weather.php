a087d946f997b56546fb59ce73e9cf799ec0aa79
uytera <uytera12@yandex.ru>
2020-05-07 13:41:56 +0500

    Множество изменений

---
+++ views/site/weather.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Weather';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="weather">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the Weather page.
+    </p>
+</div>
